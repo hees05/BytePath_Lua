be347683c8fa5dbfda61c2e0de84ab2ad04170ed
hees05 <hees05@hees.localdomain>
2025-07-31 18:03:01 +0100

    Exercise 18 completed where I bind up down left right and make it do certain actions.

--- main.lua
+++ main.lua
@@ -9,12 +9,24 @@ local input
 function love.load()
     input = Input()
     input:bind('space', 'add')
+    input:bind('up', 'up')
+    input:bind('down', 'down')
+    input:bind('right', 'right')
+    input:bind('left', 'left')
 end
 
 function love.update(dt)
-    if input:down('add', 0.25) then
-        sum = sum + 1
-        print("Sum:", sum)
+    if input:down('up', 0.25) then
+        print("Up key pressed")
+    end
+    if input:down('down', 0.25) then
+        print("down key pressed")
+    end
+    if input:down('right', 0.25) then
+        print("right key pressed")
+    end
+    if input:down('left', 0.25) then
+        print("left key pressed")
     end
 end
 
