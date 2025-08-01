253b87ec3bcd1426609489c750e746b755986f5f
hees05 <hees05@hees.localdomain>
2025-08-01 16:49:51 +0900

    Use ipairs for integer related operations using tables

--- main.lua
+++ main.lua
@@ -11,23 +11,11 @@ b = {1, 1, 3, 4, 5, 6, 7, false}
 c = {'1', '2', '3', 4, 5, 6}
 d = {1, 4, 3, 4, 5, 6}
 
-a= Map.map(a, function(v)
-    if type(v) == "number" then
-        return v*2
-    end
-    if type(v) == "string" then
-        return v .. "xD"
-    end
-    if type(v) == "boolean" then 
-        return not v
-    end
-    if type(v) == "table" then
-        return nil
-    end
-end)
-for i,v in ipairs(a) do
-    print(i,v)
+local sum = 0
+for k,v in ipairs(d) do
+    sum = sum + v
 end
+print(sum)
 
 local bool = false
 function love.load()
